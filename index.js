2ac8b5114ecf3e6113829321b30f50e37365dfec
ArturSilv4 <artur.silv4@gmail.com>
2025-07-03 21:46:29 -0300

    serzzzzza?

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const app = express();
-app.get("/", (req, res) => res.send("Bot está rodando"));
-app.listen(3000, () => console.log("🌐 Web server ativo"));
-
+require("http")
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Bot está rodando!");
+  })
+  .listen(process.env.PORT || 3000);
 
 require("dotenv").config();
 const { Client, GatewayIntentBits, Collection, Events, Partials } = require("discord.js");
